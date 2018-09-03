013c2b2ced22d53aeebd7b4755438df4532c8a96
ashish <agupta@innow8apps.com>
2018-09-03 09:55:03 +0530

    project setup with sass

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../assets/css/App.css';
+import React, { Component } from 'react';
+import '../assets/styles/main.css'
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Electron!</h1>
+        <p>this is first electron project of innow8Apps</p>
+      </div>
+    );
+  }
+}
+
+export default App;
