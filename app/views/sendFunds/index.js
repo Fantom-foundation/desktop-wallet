8232eaaf42dd05d06ffcd7301aa38b917ddf6033
Maninder <mkaur@innow8apps.com>
2018-09-25 11:50:59 +0530

    - fixes #33
    - "Total" needs to be removed from Desktop Wallet #33 done

--- app/views/sendFunds/index.js
+++ app/views/sendFunds/index.js
@@ -346,14 +346,7 @@ export default class SendFunds extends Component {
                                             <FormGroup className="mb-1">
                                                 <Input type="textarea" name="text" id="exampleText" placeholder="Optional Message" value={optionalMessage} onChange={this.setMessage.bind(this)} />
                                             </FormGroup>
-
-                                            <div className="result mt-2" style={{ maxWidth: '235px', margin: 'auto', marginRight: 0 }}>
-                                                <Row className="m-0">
-                                                    <Col className="p-0"><p><strong>Total</strong></p></Col>
-                                                    <Col className="text-right p-0"><p>{totalFees} USD <span>($100.06)</span></p></Col>
-                                                </Row>
-                                            </div>
-
+                                            <br/>
                                             <center><Button color={`${continueBtnColor}`} className="text-uppercase" onClick={this.handleCheckSend.bind(this)}>Continue</Button></center>
 
                                             <span aria-hidden
